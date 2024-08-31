0c67faccf641bd18fbd2e92e20bdf245f991d1e7
filipe <filipe@sk>
2024-08-31 01:39:40 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# python-weather-api-backend
+A Python application that queries the ClimaTempo Weather API for weather data about specific cities and regions.
+
+A valid ClimaTempo API token is necessary, and one can generate a token via: https://advisor.climatempo.com.br/home/#!/tokens
+
+it is also necessary to apply your city's flag to your token so it has permissions to send requests for weather reports for your city in the API. 
+
+This API backend will cache all weather (1 hour expiration time) and city identification info (cached indefinitely) in its memory cache, so repetated requests for the same resources won't end up consuming the API unecessarily.
+
+The main endpoints are:
+
+POST /weather/getInfo: Will query the ClimaTempo API, using the provided City ID and number of future days to be included in the weather report.
+POST /weather/getCityId: Get specific city's ID by city name and state name abbreviation. This is an utility endpoint to facilitate the search of city IDs to request weather reports.
+PUT /weather/registerCity: Register city flag for token, so token has access to the weather report for the chosen city
+
+## Technologies used:
+-Python 
+-Flask (For creating the API endpoint)
+-Flask-Restful
+-Flask-CORS
+-Python HTTP Requests 
+-Python Logging
+-Memory Caching (Caches the weather reports for avoidance of unecessary API calls for the same weather report info)
---
+++ weather_api_backend.py
@@ -0,0 +1,253 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+from urllib.parse import urlencode
+import time
+import logging
+
+app = Flask(__name__)
+CORS(app)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.StreamHandler()])
+
+# Create a logger object
+logger = logging.getLogger(__name__)
+
+# Base URL for the weather API
+BASE_URL_API = 'http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/'
+CITY_API_BASE_URL = 'http://apiadvisor.climatempo.com.br/api/v1/locale/city'
+
+# Initialize an empty cache dictionary
+weatherInfoCache = {}
+cityInfoCache = {}
+WEATHER_CACHE_EXPIRATION_TIME = 3600  # Cache expiration time for weather information by city in seconds (1 hour)
+
+@app.route('/weather/getInfo', methods=['POST'])
+def get_info():
+    try:
+        # Read the form data from the frontend request
+        data = request.form
+        headers = request.headers
+
+        # Retrieve city ID and forecast days from the form data
+        city_id = data.get("city_id", "")
+        forecast_days = data.get("forecast_days", "15")
+
+        logger.info("Incoming request for weather report for city ID: " + city_id + " for the next " + forecast_days + " days")
+
+        # Retrieve the API token from the request headers
+        token_api = headers.get('token')
+
+        if not city_id:
+            logger.error('Missing required parameter "city_id"')
+            return jsonify({'error': 'Missing required parameter "city_id"'}), 400
+        
+        if not token_api:
+            logger.error('Missing API Access Token (header "token")')
+            return jsonify({'error': 'Missing API Access Token (header "token")'}), 400
+
+        # Check cache first
+        cached_data = get_cached_weather_data(city_id, forecast_days)
+        if cached_data:
+            logger.info('Cache hit for the period %s days for City ID: %s', forecast_days, city_id)
+            logger.info("Finished processing weather report for city ID: " + city_id + " for the next " + forecast_days + " days")
+            return jsonify(cached_data)
+
+        # Construct the URL with query parameters
+        url = f"{BASE_URL_API}{city_id}/days/{forecast_days}?token={token_api}"
+
+        # Make the GET request to the API
+        response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+
+        # Log response content
+        logger.info('API Response Status Code: %d', response.status_code)
+        logger.info('API Response JSON: %s', response.json())
+
+        # Store response data in cache
+        store_weather_data(city_id, forecast_days, response.json())
+        
+        logger.info("Finished processing weather report for city ID: " + city_id + " for the next " + forecast_days + " days")
+
+        return jsonify(response.json())
+
+    except requests.exceptions.RequestException as e:
+        logger.error('Request error: %s', e)
+        response_json = response.json()
+        error_detail = response_json.get("detail", "Request error")
+        logger.error('API Response JSON: %s', response_json)
+        return jsonify({'error': error_detail, 'details': str(e)}), response.status_code
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        return jsonify({'error': 'Unexpected error', 'details': str(e)}), 500
+
+@app.route('/weather/registerCity', methods=['PUT'])
+def register_city():
+    try:
+        # Read the form data from the frontend request
+        data = request.form
+        headers = request.headers 
+
+        logger.info("Incoming request for city ID token registration")
+        # Retrieve the API token from the request headers
+        token_api = headers.get('token')
+        if not token_api:
+            logger.error('Missing API Access Token (header "token")')
+            return jsonify({'error': 'Missing API Access Token (header "token")'}), 400
+
+        # Define the URL for registering cities
+        BASE_URL_REGISTER_CITY = f'http://apiadvisor.climatempo.com.br/api-manager/user-token/{token_api}/locales'
+
+        # Retrieve and format the locale IDs from the form data
+        locale_ids = data.getlist("city_id")  # Use getlist to retrieve a list of values
+        if not locale_ids:
+            logger.error('No locale IDs provided for registration in API token')
+            return jsonify({'error': 'No locale IDs provided for registration in API token'}), 400
+
+        # Ensure all are integers
+        locale_ids = [int(id) for id in locale_ids if id.isdigit()]  
+        if not locale_ids:
+            logger.error('Invalid locale IDs provided')
+            return jsonify({'error': 'Invalid locale IDs provided'}), 400
+
+        # Prepare the data for the PUT request
+        encoded_data = urlencode({'localeId[]': locale_ids}, doseq=True)
+        
+        # Headers
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        
+        # Make the PUT request
+        response = requests.put(BASE_URL_REGISTER_CITY, headers=headers, data=encoded_data)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+        logger.info("Finished request for city ID token registration")
+        
+        return jsonify(response.json())
+
+    except requests.exceptions.RequestException as e:
+        logger.error('Request error: %s', e)
+        response_json = response.json()
+        error_detail = response_json.get("detail", "Request error")
+        logger.error('API Response JSON: %s', response_json)
+        return jsonify({'error': error_detail, 'details': str(e)}), response.status_code
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        response_json = response.json()
+        error_detail = response_json.get("detail", "Unknown error")
+        logger.error('API Response JSON: %s', response_json)
+        return jsonify({'error': 'Unexpected error', 'details': str(e)}), 500
+
+@app.route('/weather/getCityId', methods=['POST'])
+def get_city_id():
+    try:
+        # Read the form data from the frontend request
+        data = request.form
+        headers = request.headers
+
+        # Retrieve city name, state, and API token from the form data and headers
+        city_name = data.get("city_name", "")
+        state = data.get("state", "")
+        token_api = headers.get('token')
+
+        logger.info("Incoming search for city ID request for city name: " + city_name + " and state: " + state)
+    
+        if not city_name:
+            logger.error('Missing required parameter "city_name"')
+            return jsonify({'error': 'Missing required parameter "city_name"'}), 400
+        
+        if not state:
+            logger.error('Missing required parameter "state"')
+            return jsonify({'error': 'Missing required parameter "state"'}), 400
+        
+        if not token_api:
+            logger.error('Missing API Access Token (header "token")')
+            return jsonify({'error': 'Missing API Access Token (header "token")'}), 400
+
+        # Check cache first
+        cached_data = get_cached_city_data(city_name, state)
+        if cached_data:
+            logger.info("Finished search for city ID for city name: " + city_name + " and state: " + state)
+            return jsonify(cached_data)
+
+        # Construct the URL with query parameters
+        params = {
+            'name': city_name,
+            'state': state,
+            'token': token_api
+        }
+        url = f"{CITY_API_BASE_URL}?{urlencode(params)}"
+
+        # Make the GET request to the API
+        response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+
+        # Log response content for debugging
+        logger.info('API Response Status Code: %d', response.status_code)
+        logger.info('API Response JSON: %s', response.json())
+        
+        store_city_data(city_name, state, response.json())
+        
+        logger.info("Finished search for city ID for city name: " + city_name + " and state: " + state)
+
+        return jsonify(response.json())
+
+    except requests.exceptions.RequestException as e:
+        logger.error('Request error: %s', e)
+        response_json = response.json()
+        error_detail = response_json.get("detail", "Request error")
+        logger.error('API Response JSON: %s', response_json)
+        return jsonify({'error': error_detail, 'details': str(e)}), response.status_code
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        response_json = response.json()
+        error_detail = response_json.get("detail", "Unknown error")
+        logger.error('API Response JSON: %s', response_json)
+        return jsonify({'error': 'Unexpected error', 'details': str(e)}), 500
+
+def store_weather_data(city_id, forecast_days, weather_data):
+    """Store weather data in the cache."""
+    cache_key = (city_id, forecast_days)
+    weatherInfoCache[cache_key] = {
+        'data': weather_data,
+        'timestamp': time.time()
+    }
+    logger.info('Cached data for the period %s days for City ID: %s', forecast_days, city_id)
+
+def get_cached_weather_data(city_id, forecast_days):
+    """Retrieve weather data from the cache if it's still valid."""
+    cache_key = (city_id, forecast_days)
+    if cache_key in weatherInfoCache:
+        cached_data = weatherInfoCache[cache_key]
+        if time.time() - cached_data['timestamp'] < WEATHER_CACHE_EXPIRATION_TIME:
+            logger.info('Cache hit for the period %s days for City ID: %s', forecast_days, city_id)
+            return cached_data['data']
+        else:
+            del weatherInfoCache[cache_key]  # Remove expired cache
+    logger.info('Cache miss for the period %s days for City ID: %s', forecast_days, city_id)
+    return None
+
+def store_city_data(city_name, state, city_data):
+    """Store city data in the cache."""
+    cache_key = (city_name, state)
+    cityInfoCache[cache_key] = {
+        'data': city_data,
+        'timestamp': time.time()
+    }
+    logger.info('Cached data for the city: %s', city_name)
+    
+def get_cached_city_data(city_name, state):
+    """Retrieve city data from the cache if it's still valid."""
+    cache_key = (city_name, state)
+    if cache_key in cityInfoCache:
+        cached_data = cityInfoCache[cache_key]
+        logger.info('Cache hit for City: %s', city_name)
+        return cached_data['data']
+    logger.info('Cache miss for City: %s', city_name)
+    return None    
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000, debug=False)
